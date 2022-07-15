b41eaa1f7d8786af835a322a48c168464f9eb2db
ChaseCallahan37 <97930526+ChaseCallahan37@users.noreply.github.com>
2022-07-15 18:54:05 -0500

    changed course field title to name

--- Models/course-model.cs
+++ Models/course-model.cs
@@ -6,7 +6,7 @@ namespace Models
     public class CourseModel
     {
         public string Id { get; set; } = "";
-        public string Title { get; set; } = "";
+        public string Name { get; set; } = "";
 
         public string Info { get; set; } = "";
 
