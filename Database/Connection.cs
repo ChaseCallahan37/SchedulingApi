1a5115dec04bd3fdfc855c87a9e3bef5b681cd10
ChaseCallahan37 <97930526+ChaseCallahan37@users.noreply.github.com>
2022-08-12 12:03:40 -0500

    changed db from local to public

--- Database/Connection.cs
+++ Database/Connection.cs
@@ -4,8 +4,8 @@ namespace Database
 {
     public class Connection
     {
-        // public static string connectionString = "server=scheduling-db.cjmckd98ubrp.us-east-2.rds.amazonaws.com;uid=admin;pwd=13153Lakearnedra!;database=scheduling";
-        public static string connectionString = "server=127.0.0.1;uid=admin;pwd=password;database=scheduling";
+        public static string connectionString = "server=scheduling-db.cjmckd98ubrp.us-east-2.rds.amazonaws.com;uid=admin;pwd=13153Lakearnedra!;database=scheduling";
+        // public static string connectionString = "server=127.0.0.1;uid=admin;pwd=password;database=scheduling";
 
         public static MySqlConnection instance = new MySqlConnection(connectionString);
     }
